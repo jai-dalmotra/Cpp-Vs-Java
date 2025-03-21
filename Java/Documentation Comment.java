3c87406be132ace8a2ff53891bb68cd2319cb2d8
Jai Dalmotra <168071500+jai-dalmotra@users.noreply.github.com>
2025-03-21 06:18:52 +0530

    Update Documentation Comment.java

--- Java/Documentation Comment.java
+++ Java/Documentation Comment.java
@@ -1,4 +1,5 @@
 //Java provides a special type of comment (/** ... */) known as a documentation comment, which is processed by javadoc to generate HTML documentation.
+//C++ does not have a built-in documentation generator like Java's javadoc. However, tools like Doxygen can be used to generate documentation in a similar manner.
 
 /**
  * This is a sample class.

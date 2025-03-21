409b6b179e51af91edafb7d7c108e5be57d81a44
Jai Dalmotra <168071500+jai-dalmotra@users.noreply.github.com>
2025-03-21 06:17:48 +0530

    Create Documentation Comment.java

---
+++ Java/Documentation Comment.java
@@ -0,0 +1,15 @@
+//Java provides a special type of comment (/** ... */) known as a documentation comment, which is processed by javadoc to generate HTML documentation.
+
+/**
+ * This is a sample class.
+ * @author Jai Dalmotra
+ * @version 1.0
+ */
+public class Sample {
+    /**
+     * This method prints a message.
+     */
+    public void display() {
+        System.out.println("Hello, World!");
+    }
+}

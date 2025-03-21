c70f443e02d7a670b69bce6ee9674d42d130052e
Jai Dalmotra <168071500+jai-dalmotra@users.noreply.github.com>
2025-03-21 06:29:25 +0530

    Create Structures and Unions.cpp

---
+++ Cpp/Structures and Unions.cpp
@@ -0,0 +1,12 @@
+// C++: Supports structures (struct) and unions (union) for grouping data.
+//Java: Does not have struct or union, as it relies on class-based OOP.
+
+struct Student {
+    string name;
+    int age;
+};
+
+union Data {
+    int intVal;
+    float floatVal;
+};
